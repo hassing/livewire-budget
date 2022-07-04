9435795fc27b47d1081d1dbd6ef94b2b33bcb55d
Lasse Hassing <lasse@hassing.org>
2022-07-05 01:04:59 +0200

    Add spatie/laravel-backup integration

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command("backup:run --only-to-disk=s3")->dailyAt('4:00');
     }
 
     /**
